6dee832a42e80bb66dbf40def55ede1e7b7f9e19
fodisi <36540281+fodisi@users.noreply.github.com>
2018-07-08 11:15:58 -0400

    Update faucet.sol

--- faucet.sol
+++ faucet.sol
@@ -1,14 +1,14 @@
 pragma solidity ^0.4.17;
 
 //Sample Faucet contract, built based on Mastering Ethereum book.
-// Gives a max. of 0.1 ETH to the address that is requesting it.
 contract Faucet {
+    
+    // Gives a max. of 0.1 ETH to the address that is requesting it.
     function withdraw(uint _withdraw_amount) public {
         require(_withdraw_amount <= 100000000000000000);
         msg.sender.transfer(_withdraw_amount);
     }
     
+    // Standard function used to fund the contract.
     function() public payable {}
 }
-
-
